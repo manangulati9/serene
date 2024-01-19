263eabeb20f60b6013b943fe4fa769888bfd6094
Saharsh Raj <saharshraj10@gmail.com>
2024-01-19 13:30:54 +0530

    LandingSection/Disorders-readmore Completed

---
+++ src/app/Disorders/MoodSwings/page.tsx
@@ -0,0 +1,234 @@
+{/* <h2 className='font-bold text-lg'></h2>
+<h3 className='text-lg font-semibold'></h3> 
+<p className=' text-sm  text-left tracking-tighter font-base '></p>
+<ul className=' text-sm  text-left tracking-tighter font-base '></ul>*/}
+
+import React from 'react'
+import Image from 'next/image'
+
+const page = () => {
+  return (
+    <section className='bg-[#E9F8FF] flex flex-col gap-3'>
+
+    <div className='flex flex-col mt-28 px-6 md:px-8 md:mt-20 gap-2 my-16 md:my-10'>
+
+    {/* What Are Mood Swings? */}
+    <div className='md:hidden'>
+        <div>
+    <h2 className='font-bold text-lg'>What Are Mood Swings?</h2>
+
+    <p className=' text-sm  text-left tracking-tighter font-base '>Mood swings, or mood changes, are drastic fluctuations in your mood. They are sometimes due to underlying medical or psychological conditions, but they are often due to stress, your environment, and other lifestyle factors. Mood changes are often a typical part of life. Different factors can affect your mood in many ways. Stress, hormonal changes, sleep issues, and major life changes can all have a direct effect on how your mood changes.</p>
+    <p className=' text-sm  text-left tracking-tighter font-base '>Mood shifts are sometimes due to medical or psychological conditions. These types of mood changes tend to be more severe and have a greater impact on your daily life and relationships.
+This article further defines mood changes. It also talks about the causes of mood shifts and how doctors may treat them.</p>
+    <p className=' text-sm  text-left tracking-tighter font-base '>Your mood is a part of your emotional rhythm. It is generally less intense than your emotions, but it is a piece of them. It tends to have a trigger of some kind, such as an event or an experience.</p>
+    <p className=' text-sm  text-left tracking-tighter font-base '>Changes in mood are a typical part of life. Everyone experiences mood changes on a regular basis. One day, you may feel happy and upbeat, and the next, you may feel sad or irritated. Depending on what is happening in your life at the time, these types of changes are nothing more than the usual ebb and flow of your moods.</p>
+    <p className=' text-sm  text-left tracking-tighter font-base '>Mood swings tend to feel more intense than your typical mood change. They also cause more of a fluctuation in your emotions, and, often, they can last for a longer period of time.</p>
+    <p className=' text-sm  text-left tracking-tighter font-base '>Signs of a possible experience with such mood fluctuations include:</p>
+    <ul className=' text-sm  text-left tracking-tighter font-base '>
+    
+    <li>{'->'}Extremely low moods for longer than 2 weeks.</li>
+    <li>{'->'}A consistent feeling of ups and downs.</li>
+    <li>{'->'}An inability to explain why you feel a particular way.</li>
+
+    </ul>
+    <p className=' text-sm  text-left tracking-tighter font-base '>
+Just because you are experiencing mood swings does not mean that you have a serious underlying condition.</p>
+    <p className=' text-sm  text-left tracking-tighter font-base '>However, mood fluctuations can sometimes indicate conditions such as bipolar disorder, attention deficit hyperactivity disorder (ADHD), and other psychological concerns. They are also sometimes due to medical conditions such as multiple sclerosis (MS) and certain cancer treatments.</p>
+    <p className=' text-sm  text-left tracking-tighter font-base '> 
+Mood changes can just as easily result from other triggers, including stress, hormones, and life changes.
+If you feel that your mood swings are negatively affecting your daily life or relationships, you can talk with your doctor.</p>
+    </div>
+    <div className='h-fit flex justify-center my-2'>
+        <Image src={'/assets/mood_swings.png'} alt={''} className='object-contain rounded-3xl' width={320} height={160}/>
+    </div>
+    </div>
+    
+    {/* What Are Mood Swings? */}
+    <div className='hidden md:flex justify-between'>
+        <div>
+    <h2 className='font-bold text-lg'>What Are Mood Swings?</h2>
+
+    <p className=' text-sm  text-left tracking-tighter font-base '>Mood swings, or mood changes, are drastic fluctuations in your mood. They are sometimes due to underlying medical or psychological conditions, but they are often due to stress, your environment, and other lifestyle factors. Mood changes are often a typical part of life. Different factors can affect your mood in many ways. Stress, hormonal changes, sleep issues, and major life changes can all have a direct effect on how your mood changes.</p>
+    <p className=' text-sm  text-left tracking-tighter font-base '>Mood shifts are sometimes due to medical or psychological conditions. These types of mood changes tend to be more severe and have a greater impact on your daily life and relationships.
+This article further defines mood changes. It also talks about the causes of mood shifts and how doctors may treat them.</p>
+    <p className=' text-sm  text-left tracking-tighter font-base '>Your mood is a part of your emotional rhythm. It is generally less intense than your emotions, but it is a piece of them. It tends to have a trigger of some kind, such as an event or an experience.</p>
+    <p className=' text-sm  text-left tracking-tighter font-base '>Changes in mood are a typical part of life. Everyone experiences mood changes on a regular basis. One day, you may feel happy and upbeat, and the next, you may feel sad or irritated. Depending on what is happening in your life at the time, these types of changes are nothing more than the usual ebb and flow of your moods.</p>
+    <p className=' text-sm  text-left tracking-tighter font-base '>Mood swings tend to feel more intense than your typical mood change. They also cause more of a fluctuation in your emotions, and, often, they can last for a longer period of time.</p>
+    <p className=' text-sm  text-left tracking-tighter font-base '>Signs of a possible experience with such mood fluctuations include:</p>
+    <ul className=' text-sm  text-left tracking-tighter font-base '>
+    
+    <li>{'->'}Extremely low moods for longer than 2 weeks.</li>
+    <li>{'->'}A consistent feeling of ups and downs.</li>
+    <li>{'->'}An inability to explain why you feel a particular way.</li>
+
+    </ul>
+    <p className=' text-sm  text-left tracking-tighter font-base '>
+Just because you are experiencing mood swings does not mean that you have a serious underlying condition.</p>
+    <p className=' text-sm  text-left tracking-tighter font-base '>However, mood fluctuations can sometimes indicate conditions such as bipolar disorder, attention deficit hyperactivity disorder (ADHD), and other psychological concerns. They are also sometimes due to medical conditions such as multiple sclerosis (MS) and certain cancer treatments.</p>
+    <p className=' text-sm  text-left tracking-tighter font-base '> 
+Mood changes can just as easily result from other triggers, including stress, hormones, and life changes.
+If you feel that your mood swings are negatively affecting your daily life or relationships, you can talk with your doctor.</p>
+    </div>
+    <div className='h-[348px] flex justify-center my-2'>
+        <Image src={'/assets/mood_swings.png'} alt={''} className='object-contain rounded-3xl' width={400} height={200}/>
+    </div>
+    </div>
+
+    {/* What causes mood swings? */}
+    <h2 className='font-bold text-lg'>What causes mood swings?</h2>
+
+    <h3 className='text-lg font-semibold'>Bipolar Disorder</h3>
+
+    <p className=' text-sm  text-left tracking-tighter font-base '>Many people may think of bipolar disorder when they think about mood swings. While it’s true that people with that condition have mood highs and lows, it isn’t the only thing that causes them.</p>
+
+    <h3 className='text-lg font-semibold'>Sleep Deprivation</h3>
+
+    <p className=' text-sm  text-left tracking-tighter font-base '>Your brain and body recover from the events of the day while you sleep. If you don’t get enough rest, your sleep won’t fully refresh you. When you’re short on shut-eye, you might feel cranky. You’re also more likely to make poor choices throughout the day, and you may snap at people more often. If you skimp on sleep all the time, it may raise your chances of depression.
+</p>
+
+    <h3 className='text-lg font-semibold'>Low Blood Sugar
+</h3>
+
+    <p className=' text-sm  text-left tracking-tighter font-base '>
+If you’ve ever gotten “hangry” -- hungry and angry at the same time -- low blood sugar may have been to blame. This happens to some people when they go too long between meals. You may feel angry, upset, lonely, or confused. You may even want to cry or scream.
+To feel more like yourself, eat something.</p>
+    <p className=' text-sm  text-left tracking-tighter font-base '>If you have diabetes, it’s important to notice sudden mood changes because you could faint if your blood sugar stays too low for too long.
+</p>
+
+    <h3 className='text-lg font-semibold'>Stress</h3>
+
+    <p className=' text-sm  text-left tracking-tighter font-base '>Stressful situations may pop up at work, at home, or elsewhere. Chronic stress can lead to a number of health problems, and it may make you feel sad, angry, or bitter. You may lose sleep, which can affect your mood. If you can distance yourself from what causes your stress, you should start to feel more like yourself. Exercise is a good way to ease pressure, and it should also help make you feel better.</p>
+
+    <h3 className='text-lg font-semibold'>Certain Medications</h3>
+
+    <p className=' text-sm  text-left tracking-tighter font-base '>Mood swings or depression can be side effects of drugs you take. If your doctor prescribes a new medicine, pay attention to how you feel for the first few weeks, because there may be a link between your mood and your meds.
+Mood swings are a common side effect of high-dose steroids. If you take them, you may become angry more easily than usual. You might have a hard time sleeping, too. That can make your mood even worse.</p>
+
+    <h3 className='text-lg font-semibold'></h3>
+
+    <p className=' text-sm  text-left tracking-tighter font-base '></p>
+
+    <h3 className='text-lg font-semibold'></h3>
+
+    <p className=' text-sm  text-left tracking-tighter font-base '></p>
+    <p className=' text-sm  text-left tracking-tighter font-base '></p>
+
+    <h3 className='text-lg font-semibold'>Hormones</h3>
+
+    <p className=' text-sm  text-left tracking-tighter font-base '>When you take hormone therapy for different things, you may feel upset or angry for no reason. Whenever your body makes hormones in greater or smaller amounts than usual, your mood may rise or fall. The same thing can happen when your body produces surges of hormones when you go through puberty.
+</p> 
+
+    <h3 className='text-lg font-semibold'>Pregnancy</h3>
+
+    <p className=' text-sm  text-left tracking-tighter font-base '>When you’re pregnant, you make more hormones than usual to help your body grow and nourish the baby. These surges can affect your mood: You may cry more or feel empty inside. You might swing suddenly from happy to sad, then back again.
+Some women become depressed during pregnancy or after the baby is born, when hormone levels drop quickly. If that’s you, talk with your doctor so they can treat your depression and help lift your mood.</p>
+
+    <h3 className='text-lg font-semibold'>PMS</h3>
+
+    <p className=' text-sm  text-left tracking-tighter font-base '>Many women have premenstrual syndrome (PMS) during the days right before their period. This can bring symptoms like cramps, headaches, and mood changes. PMS may make you feel sad or moody without a trigger. It can be because your levels of certain hormones drop at that time of the month. Once you get your period, your hormone levels start to pick up, which helps your symptoms go away.
+</p>
+
+    <h3 className='text-lg font-semibold'>Menopause</h3>
+
+    <p className=' text-sm  text-left tracking-tighter font-base '>When you start menopause, your body makes far fewer hormones than it once did. That drop can cause any number of symptoms, including hot flashes, sleep problems, and mood swings. Lifestyle changes, like a healthier diet, more sleep, or more exercise, can help your mood. Your doctor could prescribe medicine, too.</p>
+
+    <h3 className='text-lg font-semibold'>Dementia</h3>
+
+    <p className=' text-sm  text-left tracking-tighter font-base '>Dementia causes damage to the brain, which affects a person’s memory and personality over time. People with dementia may have sudden mood swings -- calm one minute, then angry or upset the next. They may feel frustrated that they forget things or can’t express their thoughts anymore. Some people with dementia become depressed and withdraw into themselves. Others don’t interact with anyone, even if they were social before.</p>
+
+    <h3 className='text-lg font-semibold'>ADHD</h3>
+
+    <p className=' text-sm  text-left tracking-tighter font-base '>If you have attention deficit hyperactivity disorder (ADHD), you may not be able to control your impulses well. You’re likely to get angry or frustrated suddenly, even about small things like long lines or traffic. People with ADHD are more likely to become depressed or have other problems related to mood. With treatment, you can learn to control your impulses, which can help make you feel more like yourself.
+</p>
+
+    <h3 className='text-lg font-semibold'>Thyroid Issues</h3>
+
+    <p className=' text-sm  text-left tracking-tighter font-base '>People who have an overactive thyroid (hyperthyroidism) make too much thyroid hormone. People with an underactive thyroid (hypothyroidism) don’t make enough. Both can cause a number of health problems, including mood swings. When you get treatment for the thyroid, your levels should return to normal. This should help your symptoms fade, and you should start to feel more like yourself.</p>
+
+    <h3 className='text-lg font-semibold'>Caffeine</h3> 
+    <p className=' text-sm  text-left tracking-tighter font-base '>Coffee, soda, and other beverages that have caffeine can boost your mood while they give you a burst of energy. Because caffeine stimulates your nervous system, you may feel more alert than usual. If you use it often, your body gets used to its effects. By that point, if you try to cut back, it can make you feel tired, annoyed, nervous, or anxious.</p>
+
+
+    <h3 className='text-lg font-semibold'>Too Much Sugar</h3> 
+    <p className=' text-sm  text-left tracking-tighter font-base '>Many studies suggest that if you eat a lot of refined sugars, like things with high fructose corn syrup, it can affect how your brain functions. That can affect your mood. It can even make symptoms of mood disorders like depression worse. Common foods with refined sugars include crackers, flavored yogurt, tomato sauce, salad dressing, most processed foods, and many choices that are labeled “low-fat.”
+</p>
+
+
+    <h3 className='text-lg font-semibold'>Depression</h3> 
+    <p className=' text-sm  text-left tracking-tighter font-base '>Depression is a type of mood disorder. If you have it, you can feel sad and hopeless. You might lose interest in things you usually enjoy. If you've felt this way for 2 weeks or more, you should see your doctor or a mental health professional.</p>
+
+
+    
+    
+    <div className='md:hidden'>
+        <div>
+    {/* How do doctors treat mood swings? */}
+    <h2 className='font-bold text-lg'>How do doctors treat mood swings?</h2>
+
+    <p className=' text-sm  text-left tracking-tighter font-base '>Treatment for mood changes will often depend on what the underlying cause is. Your doctor will form a treatment plan for any psychological or medical condition that is the cause of your mood fluctuations.</p>
+    <p className=' text-sm  text-left tracking-tighter font-base '>There are many ways you can help manage mood changes not related to an underlying condition yourself. For example, you can try:</p>
+    <ul className=' text-sm  text-left tracking-tighter font-base '>
+    
+    <li>{'->'}Practicing mindfulness.</li>
+    <li>{'->'}Connecting with others.</li>
+    <li>{'->'}Taking time to do things you enjoy.</li>
+    <li>{'->'}Practicing self-care.</li>
+    <li>{'->'}Reducing stress.</li>
+    <li>{'->'}Adjusting your diet.</li>
+    <li>{'->'}Exercising more.</li>
+    <li>{'->'}Adjusting your sleep patterns.</li>
+    <li>{'->'}Asking for help.</li>
+
+    </ul>
+    <p className=' text-sm  text-left tracking-tighter font-base '>If you feel that you need extra help coping with your mood changes, you can contact a mental health professional. Therapy can equip you with better coping skills and ways to manage your emotions.</p>
+    </div>
+
+    <div className='h-fit flex justify-center my-2'>
+        <Image src={'/assets/mood_swings.png'} alt={''} className='object-contain rounded-3xl' width={320} height={160}/>
+    </div>
+    </div>
+    
+    <div className='hidden md:flex justify-between'>
+        <div>
+    {/* How do doctors treat mood swings? */}
+    <h2 className='font-bold text-lg'>How do doctors treat mood swings?</h2>
+
+    <p className=' text-sm  text-left tracking-tighter font-base '>Treatment for mood changes will often depend on what the underlying cause is. Your doctor will form a treatment plan for any psychological or medical condition that is the cause of your mood fluctuations.</p>
+    <p className=' text-sm  text-left tracking-tighter font-base '>There are many ways you can help manage mood changes not related to an underlying condition yourself. For example, you can try:</p>
+    <ul className=' text-sm  text-left tracking-tighter font-base '>
+    
+    <li>{'->'}Practicing mindfulness.</li>
+    <li>{'->'}Connecting with others.</li>
+    <li>{'->'}Taking time to do things you enjoy.</li>
+    <li>{'->'}Practicing self-care.</li>
+    <li>{'->'}Reducing stress.</li>
+    <li>{'->'}Adjusting your diet.</li>
+    <li>{'->'}Exercising more.</li>
+    <li>{'->'}Adjusting your sleep patterns.</li>
+    <li>{'->'}Asking for help.</li>
+
+    </ul>
+    <p className=' text-sm  text-left tracking-tighter font-base '>If you feel that you need extra help coping with your mood changes, you can contact a mental health professional. Therapy can equip you with better coping skills and ways to manage your emotions.</p>
+    </div>
+
+    <div className='h-fit flex justify-center my-2'>
+        <Image src={'/assets/mood_swings.png'} alt={''} className='object-contain rounded-3xl' width={320} height={160}/>
+    </div>
+    </div>
+
+
+    {/* Summary */}
+    <h2 className='font-bold text-lg'>Summary</h2>
+
+    <p className=' text-sm  text-left tracking-tighter font-base '>Mood swings are intense changes in your moods and emotions. They often occur due to stress, hormonal changes, and environmental factors.
+There are many ways you can manage mood shifts not caused by an underlying condition. Sometimes, however, mood fluctuations indicate a more serious psychological or medical condition that needs treatment.
+If you experience prolonged mood changes that begin to affect your daily life or relationships, you may wish to contact your doctor.</p>
+
+
+    </div>
+
+    </section>
+  )
+}
+
+export default page
